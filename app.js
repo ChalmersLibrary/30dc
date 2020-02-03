6f619bd880342c64635318251897bf13d6699c43
Lars Andersson <lars.j.andersson@chalmers.se>
2020-02-03 07:54:18 +0100

    Removed unused auth stuff.

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ var logger = require('morgan');
 var apiRouter = require("./routes/api");
 
 const fs = require("fs");
-const passport = require("passport");
-const SamlStrategy = require("passport-saml").Strategy;
-const session = require("express-session");
 
 // Load dev config if suitable.
 try {
@@ -22,35 +19,6 @@ try {
   console.error("No local config present.");
 }
 
-var SAML_CERT;
-var SAML_PRIVATE_KEY;
-
-if (process.env.USE_SAML) {
-  SAML_CERT = fs.readFileSync(process.env.CERTFILE_PATH, "utf-8");
-  SAML_PRIVATE_KEY = fs.readFileSync(process.env.KEYFILE_PATH, "utf-8");
-}
-
-const samlStrategy = new SamlStrategy({
-  callbackUrl: process.env.SAML_CALLBACK,
-  entryPoint: process.env.IDP_ENTRYPOINT,
-  issuer: process.env.SAML_ISSUER,
-  acceptedClockSkewMs: -1,
-  decryptionPvk: SAML_PRIVATE_KEY,
-  cert: [ process.env.IDP1_CERT, process.env.IDP2_CERT ],
-  identifierFormat: null
-}, (profile, done) => {
-  let profileIdPropertyName = process.env.PROFILE_PROPERTY_FOR_ID || 
-    "urn:oid:1.3.6.1.4.1.5923.1.1.1.6";
-  return done(null, { id: profile[profileIdPropertyName] })
-});
-passport.serializeUser((user, done) => {
-  done(null, user);
-});
-passport.deserializeUser((user, done) => {
-  done(null, user);
-});
-passport.use(samlStrategy);
-
 var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -60,18 +28,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.get("/saml/MetaData", (req, res) => {
-  res.type("application/xml");
-  res.send(samlStrategy.generateServiceProviderMetadata(SAML_CERT));
-});
-app.post("/login_callback", 
-  passport.authenticate("saml", {
-    failureRedirect: "login_error.html",
-    failureFlash: true
-  }),
-  (req, res) => {
-    res.redirect("/");
-  });
 app.use(express.static(path.join(__dirname, 'public')));
 app.use("/api", apiRouter);
 
